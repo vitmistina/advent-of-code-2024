846a2e4e13d1f3cb88214aa90e92d020311052fb
Vit Mistina <vit.mistina@nn.cz>
2024-12-07 13:55:25 +0100

    feat: Implement main functionality for 2024_07 challenge; add calibrator logic and unit tests

---
+++ 2024_07/aoc_2024_07.py
@@ -0,0 +1,90 @@
+from enum import Enum
+from typing import List, Optional
+
+
+class Operand(Enum):
+    ADD = 1
+    MUL = 2
+    CONCAT = 3
+
+
+class PartType(Enum):
+    PART_1 = 1
+    PART_2 = 2
+
+
+class Calibrator:
+    def __init__(self, part: PartType):
+        self.part = part
+
+    def roll_up_with_expressions(
+        self,
+        expected: int,
+        nums: List[int],
+        accumulator: Optional[int] = None,
+        operands: Optional[List[Operand]] = None,
+    ) -> Optional[List[Operand]]:
+        if accumulator is None:
+            accumulator = nums[0]
+            nums = nums[1:]
+        if operands is None:
+            operands = []
+
+        if not nums:
+            return operands if accumulator == expected else None
+
+        if accumulator > expected:
+            return None
+
+        for operand in Operand:
+            provisional = self._apply_operand(accumulator, nums[0], operand)
+            if provisional is not None:
+                result = self.roll_up_with_expressions(
+                    expected, nums[1:], provisional, operands + [operand]
+                )
+                if result is not None:
+                    return result
+        return None
+
+    def _apply_operand(
+        self, accumulator: int, num: int, operand: Operand
+    ) -> Optional[int]:
+        if operand == Operand.ADD:
+            return accumulator + num
+        elif operand == Operand.MUL:
+            return accumulator * num
+        elif operand == Operand.CONCAT:
+            return int(f"{accumulator}{num}") if self.part == PartType.PART_2 else None
+        raise ValueError(f"Invalid operand: {operand}")
+
+
+def main(input_file_path: str):
+    with open(input_file_path) as f:
+        data = [line.strip().split(":") for line in f.readlines()]
+        lines = [(int(line[0]), list(map(int, line[1].split()))) for line in data]
+
+    pt_1_calibrator = Calibrator(PartType.PART_1)
+    part_1_results = [
+        (pt_1_calibrator.roll_up_with_expressions(expected, nums), expected, nums)
+        for expected, nums in lines
+    ]
+    part_1_sum = sum(
+        expected for operator, expected, nums in part_1_results if operator is not None
+    )
+
+    pt_2_calibrator = Calibrator(PartType.PART_2)
+    part_2_results = [
+        (pt_2_calibrator.roll_up_with_expressions(expected, nums), expected, nums)
+        for operator, expected, nums in part_1_results
+        if operator is None
+    ]
+    part_2_sum = part_1_sum + sum(
+        expected for operator, expected, nums in part_2_results if operator is not None
+    )
+
+    return {"part_1": part_1_sum, "part_2": part_2_sum}
+
+
+if __name__ == "__main__":
+    result = main("./2024_07/2024_07_input.txt")
+    print(result)
---
+++ 2024_07/test_2024_07.py
@@ -0,0 +1,14 @@
+import unittest
+
+from aoc_2024_07 import main
+
+
+class Test202407(unittest.TestCase):
+    def test_day_2024_07(self):
+        result = main("./2024_07/2024_07_test.txt")
+        self.assertEqual(result["part_1"], 3749)
+        self.assertEqual(result["part_2"], 11387)
+
+
+if __name__ == "__main__":
+    unittest.main()
