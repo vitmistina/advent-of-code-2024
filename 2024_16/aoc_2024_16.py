d45e7496a60d687ccc41709ec3c25bb545e176c2
Vit Mistina <vit.mistina@nn.cz>
2024-12-16 13:06:11 +0100

    feat: Implement part 2 of 2024_16 challenge; enhance A* algorithm and update tests for dual part handling

--- 2024_16/aoc_2024_16.py
+++ 2024_16/aoc_2024_16.py
@@ -1,11 +1,17 @@
 from dataclasses import dataclass, field
+from enum import Enum
 from queue import PriorityQueue
-from typing import Dict, List, Tuple
+from typing import Dict, List, Set, Tuple
 
 Coord = Tuple[int, int]
 Direction = Tuple[int, int]
 VisitedKey = Tuple[Coord, Direction]
 QueueItem = Tuple[List[Coord], List[Direction], int]
+
+
+class PartType(Enum):
+    PART_1 = 1
+    PART_2 = 2
 
 
 @dataclass(order=True)
@@ -18,13 +24,14 @@ DIRECTIONS: List[Direction] = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
 
 class Map:
-    def __init__(self, data: List[List[str]]):
+    def __init__(self, data: List[List[str]], part: PartType):
         self.map = data
         self.start = (1, len(data) - 2)
         self.end = (len(data[0]) - 2, 1)
+        self.part = part
 
     def a_star(self) -> int:
-        minimums: Dict[VisitedKey, int] = {}
+        minimums: Dict[VisitedKey, Tuple[int, Set[Coord], int]] = {}
         queue: PriorityQueue[QueueItem] = PriorityQueue()
         queue.put(PrioritizedItem(0, ([self.start], [(1, 0)], 0)))
 
@@ -41,32 +48,39 @@ class Map:
             candidates = [
                 ((current_pos[0] + d[0], current_pos[1] + d[1]), d) for d in DIRECTIONS
             ]
-            # if queue.qsize() == 0:
-            #     print("Last item")
-            #     print(current_pos, current_dir, distance)
-            #     self.print_with_history(history)
-            #     # print(directions)
+            end_is_near = None
             for candidate, new_dir in candidates:
                 x, y = candidate
                 if self.map[y][x] == "#":
                     continue
-                # if candidate in history:
-                #     continue
-                prev_min = minimums.get((candidate, new_dir), float("inf"))
+                prev_min, prev_min_history, ways_to_get_there = minimums.get(
+                    (candidate, new_dir), (float("inf"), set(), 0)
+                )
                 rotation_cost = 0 if current_dir == new_dir else 1000
                 new_distance = distance + 1 + rotation_cost
-                # print(history + [candidate])
-                # self.print_with_history(history + [candidate])
-                if new_distance < prev_min:
-                    minimums[(candidate, new_dir)] = new_distance
-                    new_history = list(history) + [candidate]
+                if end_is_near and new_distance != end_is_near:
+                    continue
+                new_history = list(history) + [candidate]
+
+                if new_distance < prev_min or (
+                    self.part == PartType.PART_2
+                    and new_distance == prev_min
+                    and set(new_history) - prev_min_history != set()
+                ):
+                    minimums[(candidate, new_dir)] = (
+                        new_distance,
+                        set(new_history) | prev_min_history,
+                        ways_to_get_there + 1,
+                    )
                     new_directions = list(directions) + [new_dir]
                     if candidate == self.end:
                         ends[new_dir] = new_distance
                         if all(ends.values()):
-                            return min(ends.values())
+                            if self.part == PartType.PART_1:
+                                return min(ends.values())
+                            else:
+                                end_is_near = new_distance
                     else:
-                        # queue.append((new_history, new_directions, new_distance))
                         heuristic = abs(candidate[0] - self.end[0]) + abs(
                             candidate[1] - self.end[1]
                         )
@@ -76,15 +90,11 @@ class Map:
                                 (new_history, new_directions, new_distance),
                             )
                         )
-                        # if candidate[0] % 10 == 0 or candidate[1] % 10 == 0:
-                        # print(
-                        #     candidate,
-                        #     new_distance,
-                        #     len(new_history),
-                        #     len(new_directions),
-                        #     queue.qsize(),
-                        # )
-                        # print(candidate, new_distance)
+        if self.part == PartType.PART_2:
+            optimal_path_distance = min(ends.values())
+            for direction, distance in ends.items():
+                if distance == optimal_path_distance:
+                    return len(minimums[(self.end, direction)][1])
         raise ValueError("No path found")
 
     def print_with_history(self, history: List[Coord]):
@@ -101,9 +111,11 @@ class Map:
 def main(input_file_path: str):
     with open(input_file_path) as f:
         data = [list(line.strip()) for line in f.readlines()]
-        map = Map(data)
+        map = Map(data, PartType.PART_1)
         part_1 = map.a_star()
-        return {"part_1": part_1, "part_2": None}
+        map = Map(data, PartType.PART_2)
+        part_2 = map.a_star()
+        return {"part_1": part_1, "part_2": part_2}
 
 
 if __name__ == "__main__":
--- 2024_16/test_2024_16.py
+++ 2024_16/test_2024_16.py
@@ -7,8 +7,10 @@ class Test202416(unittest.TestCase):
     def test_day_2024_16(self):
         result = main("./2024_16/2024_16_test.txt")
         self.assertEqual(result["part_1"], 7036)
+        self.assertEqual(result["part_2"], 45)
         result = main("./2024_16/2024_16_test_2.txt")
         self.assertEqual(result["part_1"], 11048)
+        self.assertEqual(result["part_2"], 64)
 
 
 if __name__ == "__main__":
